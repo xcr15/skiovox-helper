04cb25e5518b2a9bb4f2fea5b77e5b8a40a2941f
xcr15 <120267628+xcr15@users.noreply.github.com>
2023-11-10 17:02:35 -0500

    Update main.js
    added a help URL with everything they could possibly need, if you're broke on the streets thatll save them.

--- new-tab/main.js
+++ new-tab/main.js
@@ -1,8 +1,9 @@
 import { DraggableElement } from "./draggable-element.js";
 import { FullscreenElement } from "./fullscreen-element.js";
+import { HTMLContent } from "./html-content.js"; 
 
 const FILES_URL = "chrome://file-manager";
-const HELP_URL = "data:,Someone remind Bypassi to add a help URL"; // TODO
+const HELP_URL = `data:text/html,${HTMLContent.initilize()}`; // TODO
 const WEBSTORE_URL = "https://chromewebstore.google.com";
 const ADDSESSION_URL = "https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&flowName=GlifWebSignIn&flowEntry=AddSession";
 
@@ -27,12 +28,13 @@ help.addEventListener('click', () => {
 
 webStore.addEventListener('click', () => {
     let version = Number(navigator.appVersion.match(/Chrom(e|ium)\/([0-9]+)/)[2]);
-    if (version < 112) { // not sure if this is actually the version
+    if (version <= 112) {
+        // not sure if this is actually the version
         alert("This web store may not supported by your version");
     }
 
     chrome.tabs.create({ url: WEBSTORE_URL })
-})
+});
 
 addAccount.addEventListener('click', () => {
     chrome.tabs.create({ url: ADDSESSION_URL })
